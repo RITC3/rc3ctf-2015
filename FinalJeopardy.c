221a5cf8f77389776e658a9e432228f4ae6f10c1
jgeigerm <jmg2967@rit.edu>
2015-10-27 01:59:16 -0400

    finished challenge

--- FinalJeopardy.c
+++ FinalJeopardy.c
@@ -21,6 +21,15 @@
 #include <time.h>
 #include <fcntl.h>
 
+void dot_sleep(int sec){
+    for (int i=0;i<=sec;++i){
+        sleep(1);
+        printf(".");
+        fflush(stdout);
+    }
+    printf(" ");
+}
+
 void print_flag(){
     char flagbuf[14] = {0};
     int fd = open("flag.txt", O_RDONLY);
@@ -37,36 +46,41 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     long long number;
     int moneys = 100;
-    long long wager;
+    long wager;
     long long long_answer = ((long)rand() << 32) | (long)rand();
     if (long_answer > 10) long_answer+=11;
     puts("Lex Trebeq: And now it's time for Final Jeopardy!");
+    sleep(3);
     puts("            The current scores are: IBM Watson with $102,600, Stephen Hawking with $110,400, and YOU with an astonishing $100.");
+    sleep(3);
     puts("            The category is 'Numbers greater than 10'");
+    sleep(3);
     printf("Enter your answer: ");
     scanf("%lld", &number);
     printf("Enter your wager: ");
-    scanf("%lld", &wager);
-    if ((int)wager >= 100 || wager < 0){
+    scanf("%ld", &wager);
+    if ((int)wager > 100 || wager < 0){
         puts("You can't even bet that much money...");
         return 0;
     }
-    //sleep(5);
+    dot_sleep(8);
     puts("And the answers are in!");
+    sleep(3);
     if (number == long_answer){
-        puts("Wow good job you weren't supposed to be able to do that!!");
+        puts("Wow good job you weren't even supposed to be able to do that!!");
         moneys += wager;
     } else {
         moneys -= wager;
+        printf("I'm sorry %lld was not the right answer. It was actually %lld.\n", number, long_answer);
     }
+    sleep(3);
     if (moneys <= 110400){
-        printf("You only have $%d. You lose to Stephen Hawking with $110,400\n", moneys);
+        printf("It looks like despite your answer there was no way for you to win. You lose! (%d)", moneys);
     } else {
-        printf("Moneys: %d\nWager (int): %d\n", moneys, (int)wager);
-        puts("WHAT HOW DOES THAT WORK?? YOU WIN CHARLIE!");
+        dot_sleep(5);
+        printf("Although it looks like through some miracle, you have won with $%d... HOW DOES THAT WORK??\nAnyway you'll be going home with one of our fabulous FLAGS!!!!!\n", moneys);
+        sleep(5);
         print_flag();
     }
     return 0;
 }
-
-
